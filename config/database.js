c49cd461057eda0e4afdd987b35ff6e845635cc6
yastamardika <rattyasta1@gmail.com>
2021-05-16 20:57:22 +0700

    modify db config

--- config/database.js
+++ config/database.js
@@ -6,6 +6,8 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const URL = require("url-parse");
+const PROD_MYSQL_DB = new URL(Env.get("CLEARDB_DATABASE_URL"));
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -50,12 +52,13 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', PROD_MYSQL_DB.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      user: Env.get('DB_USER', PROD_MYSQL_DB.username),
+      password: Env.get('DB_PASSWORD', PROD_MYSQL_DB.password),
+      database: Env.get('DB_DATABASE', PROD_MYSQL_DB.pathname.substr(1))
+    },
+    debug: Env.get("DB_DEBUG", false)
   },
 
   /*
