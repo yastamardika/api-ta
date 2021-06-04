5ea984f2b6fe4265140e91e1a18eb0be3ba4fa5f
yastamardika <rattyasta1@gmail.com>
2021-06-04 23:25:52 +0700

    add order relations

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -4,6 +4,27 @@
 const Model = use('Model')
 
 class Order extends Model {
+    customer (){
+        return this.hasOne('App/Models/User','userId', 'id')
+    }
+    status (){
+        return this.hasOne('App/Models/Payment','sanggar_addressId', 'id')
+    }
+    payment (){
+        return this.hasOne('App/Models/Payment','paymentId', 'id')
+    }
+    sanggar (){
+        return this.hasOne('App/Models/Sanggar','sanggarId', 'id')
+    }
+    venue(){
+        return this.hasOne('App/Models/DetailVenue','venueId', 'id')
+    }
+    detail (){
+        return this.hasOne('App/Models/OrderDetail','order_detailId', 'id')
+    }
+    package(){
+        return this.hasOne('App/Models/DancePackage','packageId', 'id')
+    }
 }
 
 module.exports = Order
