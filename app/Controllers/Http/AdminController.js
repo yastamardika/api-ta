ec0deb1a36baa732ce7e998c517acb8df2aac022
yastamardika <rattyasta1@gmail.com>
2021-06-04 23:25:24 +0700

    add order functions

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -4,15 +4,37 @@ const User = use("App/Models/User");
 const Database = use('Database')
 const Persona = use("Persona");
 class AdminController {
-    
-    verifyPartner({auth,params, response }){
-        const currentUser = await auth.getUser()
-        const toBePartner = await User.find(params.id)
-        if(currentUser.role === 'admin'){
-          toBePartner.update('role', 'partner')
-          return response.status(200).json({message : "success",data : toBePartner})
-        }
-        return response.status(400).json({message : "failed"})
-      }
- 
+
+  async indexOrderAdmin({ response }) {
+    try {
+      const order = Order.query()
+      .with('customer')
+      .with('package')
+      .with('detail')
+      .with('venue')
+      .with('sanggar')
+      .with('payment')
+      .with('status')
+      .fetch();
+      response.status(200).json({ message: "success!", data: order });
+    } catch (error) {
+      response.status(500).json({ message: error });
+    }
+  }
+
+  async detailOrderAdmin({ params, response }) {
+    try {
+      const order = Order.query().where("id", params.orderId) 
+      .with('customer')
+      .with('package')
+      .with('detail')
+      .with('venue')
+      .with('sanggar')
+      .with('payment')
+      .with('status').fetch();
+      response.status(200).json({ message: "success!", data: order });
+    } catch (error) {
+      response.status(500).json({ message: error });
+    }
+  }
 }
