9ce64359e33eda2ec3a224202911e958e25cd331
yastamardika <rattyasta1@gmail.com>
2021-06-21 21:33:04 +0700

    Delete all payment modules

--- app/Models/PaymentStatus.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class PaymentStatus extends Model {
-}
-
-module.exports = PaymentStatus
